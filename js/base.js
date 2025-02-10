b3b4d9c476b262d0126fc16f95f577d44595a56e
Vittorio <viegoxaspar@gmail.com>
2025-02-10 10:41:31 -0300

    01- criando o arquivo index.html e o arquivo inical do JS

---
+++ js/base.js
@@ -0,0 +1,32 @@
+// Exibir o console
+// console só existe no navegador
+console.log('Olá, sou o console');
+console.log('Eu exibo o conteúdo');
+
+// Variáveis
+// sempre que mudar será um let
+let rmAluno = 666;
+let nomeAluno = 'Rivaldo Jesus';
+let turmaAluno = '3TI';
+
+// quando não mudar sera uma const (constante)
+const ESCOLA_ALUNO = 'Lourdão'
+
+console.log(rmAluno, nomeAluno);
+console.log(turmaAluno, ESCOLA_ALUNO);         
+
+// concatenar/concatenação = pessima pratica
+console.log('Nome do aluno: ' + nomeAluno);
+
+// template string
+console.log(`Nome do aluno: ${nomeAluno} Rm do aluno: ${rmAluno} `);
+
+ turmaAluno = '3A';
+ console.log(`Turma do aluno: ${turmaAluno}`);
+
+
+
+
+
+
+ 
